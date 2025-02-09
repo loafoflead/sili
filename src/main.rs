909a2b0b6d848f01c372f33da5854987b0c0dd67
Ben <sopwithplays@gmail.com>
2025-02-09 15:12:28 +0100

    reinventing has never been so 'the wheel'. Implemented a third of the functionality of the 'add' instruction.

--- src/main.rs
+++ src/main.rs
@@ -59,16 +59,59 @@ fn read_line() -> Result<String, io::Error> {
 #[derive(Debug)]
 enum MachineError {
     SyntaxError(Location),
+    CannotWriteTo(Token),
+    CannotWriteToMemory(Memory),
+    CannotReadFrom(Token),
     InvalidDest(Token),
+    UnkownRegister(String),
+    ParsingError,
 }
 
 impl Display for MachineError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> Result<(), fmt::Error> {
         match self {
-            Self::SyntaxError(loc) => write!(f, "Syntax error at: {loc}"),
+            Self::SyntaxError(loc) => write!(f, "{loc}: Syntax error..."),
+            Self::UnkownRegister(reg) => write!(f, "(?TODO?): Unknown register: {reg}"),
+            Self::CannotWriteTo(token) => write!(f, "{loc}: Invalid write operation destination, expected register or memory; found: {:?}", token.ty, loc = token.loc),
+            Self::CannotWriteToMemory(mem) => write!(f, "(?TODO?): Invalid write operation destination, expected register or pointer; found: {:?}", mem.ty),
+            Self::CannotReadFrom(token) => write!(f, "{loc}: Invalid read operation source, expected register, memory, or constant; found: {:?}", token.ty, loc = token.loc),
             Self::InvalidDest(token) => write!(f, "{loc}: Invalid operation destination: {:?}", token.ty, loc = token.loc),
+            Self::ParsingError => todo!("Implement parsing error handling."),
         }
     }
+}
+
+#[derive(Debug, Clone)]
+struct Memory {
+    ty: MemoryType,
+}
+
+impl Memory {
+    fn is_writable(&self) -> bool {
+        match self.ty {
+            MemoryType::Reg(_) | MemoryType::Ptr(_) => true,
+            MemoryType::UnsizedConstant(_) => false,
+        }
+    }
+
+    fn reg(name: impl ToString) -> Self {
+        Self {
+            ty: MemoryType::Reg(name.to_string())
+        }
+    }
+
+    fn usz(usz: usize) -> Self {
+        Self {
+            ty: MemoryType::UnsizedConstant(usz)
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+enum MemoryType {
+    Reg(String),
+    Ptr(usize),
+    UnsizedConstant(usize),
 }
 
 // Virtual machine ish thing
@@ -96,6 +139,34 @@ impl<const N: usize> Machine<N> {
         }
     }
 
+    fn to_writable(&self, token: &Token, deref: bool) -> Result<Memory, MachineError> {
+        match &token.ty {
+            TokenType::Ident(name) => {
+                let _ptr = self.get_reg(name.as_str()).ok_or_else(|| todo!("Implement constants."))?;
+                if deref { todo!("Implement dereferencing ptrs."); }
+                // just a matter of using the value of get_reg i guess.
+
+                Ok(Memory::reg(name.as_str()))
+            }
+            _ => Err(MachineError::CannotWriteTo(token.clone()))
+        }
+    }
+
+    fn to_readable(&self, token: &Token, deref: bool) -> Result<Memory, MachineError> {
+        match &token.ty {
+            TokenType::Ident(name) => {
+                let _ptr = self.get_reg(name.as_str()).ok_or_else(|| todo!("Implement constants."))?;
+                if deref { todo!("Implement dereferencing ptrs."); }
+
+                Ok(Memory::reg(name.as_str()))
+            }
+            TokenType::IntLiteral(int) => {
+                Ok(Memory::usz(*int as usize))
+            }
+            _ => Err(MachineError::CannotReadFrom(token.clone()))
+        }
+    }
+
     fn get_reg(&self, s: &str) -> Option<usize> {
         match s {
             "rip" => self.rip,
@@ -107,6 +178,7 @@ impl<const N: usize> Machine<N> {
         }.some()
     }
 
+    // https://doc.rust-lang.org/std/mem/fn.transmute.html
     fn set_reg(&mut self, s: &str, val: usize) -> Option<()> {
         *(match s {
             "rip" => &mut self.rip,
@@ -138,35 +210,28 @@ impl<const N: usize> Machine<N> {
             string_enter: '<',
             string_exit: '>',
         };
-        let tokens = ast.parse(code).expect("Syntax error");
+        let tokens = ast.parse(code).ok_or(MachineError::ParsingError)?;
         let mut bytecode: Vec<u8> = vec![];
-
-        dbg!(&tokens);
 
         let mut i = 0;
 
         while let Some(Token {ty, loc}) = tokens.get(i) {
-            i += 1; // consume the instruction
-
             use TokenType as T;
             match ty {
                 T::Keyword(kw) => {
-                    println!("Keyword {kw:?} found.");
+                    i += 1;
                     match kw {
                         AsmKeyword::Mov => todo!("mov"),
                         // Accepted syntax:
                         // add reg const|reg|[ptr] (add rin 10) -- 2 or 4 tokens
                         // add [ptr] const|reg|[ptr] (deref)    -- 4 or 6 token
                         AsmKeyword::Add => {
-                            let newline = tokens
+                            let tokens_taken = tokens[i..]
                                 .iter()
                                 .position(|t| t.ty == TokenType::Newline || t.ty == TokenType::Eof)
                                 .expect("Infinite input error! :O"); // this shouldn't ever fail
-                            let tokens_taken = newline - i;
 
-                            dbg!(newline);
-                            dbg!(&tokens[i..newline]);
-                            dbg!(&tokens[i]);
+                            dbg!(i, tokens_taken);
 
                             if tokens_taken < 2 || tokens_taken == 3 || tokens_taken == 5 {
                                 // TODO: TooFewArgs
@@ -177,9 +242,10 @@ impl<const N: usize> Machine<N> {
                                 todo!("Implement dereferencing pointers");
                                 // (dest, 3)
                             }
-                            else {
-                                self.assert_is_reg(&tokens[i])?;
-                                (&tokens[i], 1)
+                            else if let Ok(writable) = self.to_writable(&tokens[i], false) {
+                                (writable, 1)
+                            } else {
+                                return Err(MachineError::SyntaxError(loc.clone()));
                             };
 
                             i += n;
@@ -188,25 +254,25 @@ impl<const N: usize> Machine<N> {
                                 todo!("Implement dereferencing pointers");
                                 // (dest, 3)
                             }
-                            else if Ok(_) = self.assert_is_reg(&tokens[i]) {
-                                (&tokens[i], 1)
-                            }
-                            else if let TokenType::IntLiteral(_) = &tokens[i] {
-                                (&tokens[i], 1)
-                            }
-                            else {
-                                todo!("Implement constants");
+                            else if let Ok(readable) = self.to_readable(&tokens[i], false) {
+                                (readable, 1)
+                            } else {
+                                return Err(MachineError::SyntaxError(loc.clone()));
                             };
                             
                             i += n;
 
-                            
+                            let cur = self.read_mem(&dest);
+                            let to_add = self.read_mem(&src);
+                            self.write(&dest, &Memory::usz(cur + to_add))?;
                         }
                         AsmKeyword::Ret => {
                             bytecode.push(kw.to_bytecode());
                         }
                     }
                 }
+                T::Newline => i += 1,
+                T::Eof => break,
                 tok => todo!("Implement {tok:?}")
             }
         }
@@ -214,8 +280,49 @@ impl<const N: usize> Machine<N> {
         Ok(())
     }
 
+    fn read_mem(&self, mem: &Memory) -> usize {
+        use MemoryType as Mt;
+        match &mem.ty {
+            Mt::Reg(name) => self.get_reg(name.as_str()).expect("Memory struct was invalid."),
+            Mt::Ptr(_addr) => todo!("Read ptr"),
+            Mt::UnsizedConstant(cons) => *cons
+        }
+    }
+
+    fn write(&mut self, dest: &Memory, src: &Memory) -> Result<(), MachineError> {
+        use MemoryType as Mt;
+        if !dest.is_writable() {
+            return Err(MachineError::CannotWriteToMemory(dest.clone()));
+        }
+        match (&dest.ty, &src.ty) {
+            (Mt::Reg(name), Mt::UnsizedConstant(int)) => {
+                self.set_reg(name.as_str(), *int as usize);
+            }
+            (Mt::Reg(name_dest), Mt::Reg(name_src)) => {
+                let src = self.get_reg(name_src.as_str()).ok_or(MachineError::UnkownRegister(name_src.clone()))?;
+
+                self.set_reg(name_dest.as_str(), src);
+            }
+            (Mt::Reg(_name_dest), Mt::Ptr(_src_addr)) => {
+                todo!("Writing to register from memory");
+            }
+            // writing to memory
+            (Mt::Ptr(_dest_addr), Mt::UnsizedConstant(_int)) => {
+                todo!("Writing to memory");
+            }
+            (Mt::Ptr(_dest_addr), Mt::Reg(_name)) => {
+                todo!("Writing from register to memory");
+            }
+            (Mt::Ptr(_dest_addr), Mt::Ptr(_src_addr)) => {
+                todo!("Writing from memory to memory");
+            }
+            _ => unreachable!("is_writable check should prevent this.")
+        }
+        Ok(())
+    }
+
     fn read_byte(&self, loc: usize) -> Option<u8> {
-        if loc >= N || loc < 0 {
+        if loc >= N {
             return None;
         }
         self.memory[loc].some()
@@ -234,6 +341,7 @@ impl Ast {
         let chars = string.chars().collect::<Vec<char>>();
 
         let mut line = 0;
+        let mut column = 0;
 
         let mut tokens: Vec<Token> = vec![];
 
@@ -244,7 +352,7 @@ impl Ast {
             if in_string {
                 if c == self.string_exit {
                     in_string = false;
-                    tokens.push(Token::ty(TokenType::StringLiteral(buf.drain(..).collect::<_>()), line));
+                    tokens.push(Token::ty(TokenType::StringLiteral(buf.drain(..).collect::<_>()), line, column));
                 }
                 else {
                     buf.push(c);
@@ -254,14 +362,15 @@ impl Ast {
                 if c == '\n' {
                     // TODO: extract this horrible logic :( i don't like repeating it
                     if !buf.trim().is_empty() {
-                        tokens.push(Token::new(buf.as_str().trim(), line));
+                        tokens.push(Token::new(buf.as_str().trim(), line, column));
                         buf.clear();
                     }
-                    tokens.push(Token::new("\n", line));
+                    tokens.push(Token::new("\n", line, column));
                     line += 1;
+                    column = 0;
                 }
                 else {
-                    tokens.push(Token::new(buf.as_str().trim(), line));
+                    tokens.push(Token::new(buf.as_str().trim(), line, column));
                 }
                 buf.clear();
             }
@@ -270,21 +379,22 @@ impl Ast {
             }
             else if self.puncts.contains(&c) {
                 if !buf.trim().is_empty() {
-                    tokens.push(Token::new(buf.as_str().trim(), line));
+                    tokens.push(Token::new(buf.as_str().trim(), line, column));
                     buf.clear();
                 }
-                tokens.push(Token::ty(TokenType::Punct(c), line));
+                tokens.push(Token::ty(TokenType::Punct(c), line, column));
             }
             else if c == '\0' {
                 if !buf.trim().is_empty() {
-                    tokens.push(Token::new(buf.as_str().trim(), line));
+                    tokens.push(Token::new(buf.as_str().trim(), line, column));
                     buf.clear();
                 }
-                tokens.push(Token::ty(TokenType::Eof, line));
+                tokens.push(Token::ty(TokenType::Eof, line, column));
             }
             else {
                 buf.push(c);
             }
+            column += 1;
         };
 
         for c in chars {
@@ -311,17 +421,17 @@ struct Token {
 }
 
 impl Token {
-    fn new(text: &str, line: usize) -> Self {
+    fn new(text: &str, line: usize, column: usize) -> Self {
         Self {
             ty: TokenType::from_str(text),
-            loc: Location::line(line),
+            loc: Location::new(line, column),
         }
     }
 
-    fn ty(ty: TokenType, line: usize) -> Self {
+    fn ty(ty: TokenType, line: usize, column: usize) -> Self {
         Self {
             ty,
-            loc: Location::line(line),
+            loc: Location::new(line, column),
         }
     }
 }
@@ -329,17 +439,18 @@ impl Token {
 #[derive(Debug, Clone)]
 struct Location {
     line: usize,
+    column: usize,
 }
 
 impl Display for Location {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> Result<(), fmt::Error> {
-        write!(f, "{}:{}", self.line, "?")
+        write!(f, "{}:{}", self.line, self.column)
     }
 }
 
 impl Location {
-    fn line(line: usize) -> Self {
-        Self { line }
+    fn new(line: usize, column: usize) -> Self {
+        Self { line, column }
     }
 }
 
@@ -379,13 +490,19 @@ fn main() {
     // add rdi 10   -> adds 10 to the value of rdi
 
     // loop {
-        let input = "add rin 10\nret";//read_line().expect("Realistically a really bad error.");
+        let reg = "rin";
+        let input = "add rout 12\nadd rin rout\nret";//read_line().expect("Realistically a really bad error.");
+
+        println!("Register at {reg}: {byte}", byte=machine.get_reg(reg).expect("Tried to read unknown register"));
 
         if let Err(e) = machine.assemble(input) {
             println!("Error reading input: {}", e);
         }
+
+        println!("Running the following code: \n{input}");
         
         let read_loc = 0x0;
         println!("Byte at {read_loc:#x}: {byte:#x}", byte=machine.read_byte(read_loc).expect("Out of bounds memory access."));
+        println!("Register at {reg}: {byte}", byte=machine.get_reg(reg).expect("Tried to read unknown register"));
     // }
 }
